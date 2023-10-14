b61b0f7e964607e0476ebe4dbf97239a0703fac0
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-14 09:12:17 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ main() {
     echo -e "hydrocxrbon generator install script by norb_"
     echo -e "Downloading current release..."
     curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/release.js" -o "./index.js"
-curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/release.jsc" -o "./source.jsc"
     curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/configtemplate.json" -o "./config.json"
     echo -e "Installing dependencies (may take a while)"
     npm i @devnorb/chatgpt puppeteer-extra puppeteer-extra-plugin-stealth unique-names-generator @cliqz/adblocker-puppeteer discord.js
