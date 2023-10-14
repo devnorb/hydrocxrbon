d57b43f29d9926fa32be570accb8b36fd38a1a72
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-13 20:15:03 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,14 @@
 main() {
     echo -e "This IS A BETA RELEASE of hydrocxrbon! Join dsc.gg/thegenhub for more information."
     echo -e "hydrocxrbon generator install script by norb_"
-    echo -e "Undergoing development (something broke)"
-#     echo -e "Downloading current release..."
-#     curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/release.js" -o "./index.js"
-# curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/release.jsc" -o "./source.jsc"
-#     curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/configtemplate.json" -o "./config.json"
-#     echo -e "Installing dependencies (may take a while)"
-#     npm i chatgpt puppeteer-extra puppeteer-extra-plugin-stealth unique-names-generator @cliqz/adblocker-puppeteer discord.js
-#     echo -e "Finished hydrocxrbon installation."
-#     echo -e "norb_ on top - join dsc.gg/thegenhub"
+    echo -e "Downloading current release..."
+    curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/release.js" -o "./index.js"
+curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/release.jsc" -o "./source.jsc"
+    curl "https://raw.githubusercontent.com/devnorb/discordAccountGenerator/main/configtemplate.json" -o "./config.json"
+    echo -e "Installing dependencies (may take a while)"
+    npm i @devnorb/chatgpt puppeteer-extra puppeteer-extra-plugin-stealth unique-names-generator @cliqz/adblocker-puppeteer discord.js
+    echo -e "Finished hydrocxrbon installation."
+    echo -e "norb_ on top - join dsc.gg/thegenhub"
 }
 
 main
