accebd3e8ded323daec77879898b913c56959516
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-16 21:20:03 -0700

    v1.1

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ main() {
     echo -e "hydrocxrbon is being developed. | try again later."
     curl "https://raw.githubusercontent.com/devnorb/hydrocxrbon/main/main.js" -o "./main.js"
     curl "https://raw.githubusercontent.com/devnorb/hydrocxrbon/main/configtemplate.json" -o "./config.json"
+    mkdir screenshotLogs
     echo -e "Installing dependencies (may take a while)"
     npm i @devnorb/chatgpt puppeteer-extra puppeteer-extra-plugin-stealth unique-names-generator @cliqz/adblocker-puppeteer discord.js
     echo -e "Finished hydrocxrbon installation."
